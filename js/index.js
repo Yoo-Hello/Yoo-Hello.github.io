4c988b51059f6b5d10f689ca32564cb8a6c4b435
Yoo-Hello <765711543@qq.com>
2016-03-23 22:00:44 +0800

    修改了动画效果

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 $(function(){
-	var times=function(){
+	var n=0;
+	var nexts=function(){
 		var w=$('.banner').width();
 		$('.banner:first')
 		.animate({left:-w},function(){
@@ -10,5 +11,28 @@ $(function(){
 		});
 		$('.banner:first').next().animate({left:0})
 	}
-	var move=setInterval(times,1000);
+	var move=setInterval(nexts,5000);
+	var previous=function(){
+		var w=$('.banner').width();
+		console.log($('.banner:last'))
+		$('.banner:last')
+		.css('left','-100%')
+		.parent('.banner-box')
+		.prepend($('.banner:last'))
+		.find('div:first')
+		.animate({left:0});
+		$('.banner:first').next().animate({left:+w})
+	}
+	$('.banner').mouseover(function(){
+		clearInterval(move);
+	}).mouseout(function(){
+		move=setInterval(nexts,5000);
+	})
+
+	$('.Lanniu').click(function(){
+		previous();
+	})
+	$('.Ranniu').click(function(){
+		nexts();
+	})
 })
